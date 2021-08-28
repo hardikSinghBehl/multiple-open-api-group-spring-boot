a7ed6d568d9b500831d8b03acb2904a9e6740723
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-08-28 09:05:28 +0530

    init project commit

---
+++ src/main/java/com/behl/agares/MultipleOpenApiInstanceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.behl.agares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleOpenApiInstanceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleOpenApiInstanceSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/behl/agares/MultipleOpenApiInstanceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.behl.agares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleOpenApiInstanceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
